ca3d4fc7fd01b2cda412273919cefaed1ceec2df
jl <00.suptw@gmail.com>
2021-03-16 18:36:20 -0700

    userInterface fix

--- src/com/company/UserInterface.java
+++ src/com/company/UserInterface.java
@@ -1,33 +1,36 @@
 package com.company; /** Course Project CS 22c
-*Autor: Chengyun Li
-*/
+ *Autor: Chengyun Li
+ */
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 
 public class UserInterface {
-	
+
 	public static void main(String[] args) throws IOException {
 
 		final int NUM_ACCOUNT = 30;
-		final  int NUM_USERS = 30;
+		final int NUM_USERS = 30;
 		final int NUM_INTEREST= 20;
 		int userId = -1;
 		HashTable<UserAccount> userAccounts= new HashTable<>(NUM_ACCOUNT);
 		HashTable<User> userNames = new HashTable<>(NUM_USERS);
 		HashTable<Interest> interest = new HashTable<>(NUM_INTEREST);
 		ArrayList<BST<User>> NewFriendByInterest = new ArrayList<BST<User>>();
+		ArrayList<String> tempUsers = new ArrayList<String>();
+		ArrayList<List<String>> tempFriends = new ArrayList<List<String>>();
 		// output: 
 		ArrayList<String> outputNames = new ArrayList<>();
 		for(int i = 0; i < NUM_INTEREST; i++) {
 			NewFriendByInterest.add(new BST<User>());
 		}
-		 
-		File file = new File("princessesv2.txt");
+
+		File file = new File("PrincessesV2.txt");
 		//reading input file
 		try {
 			Scanner input = new Scanner(file);
-			while (input.hasNextLine()) { 
+			while (input.hasNextLine()) {
+				int userIndex = 0;
 				String[] str = new String[35];
 				for(int i = 0; i < 35; i++) {
 					if (input.hasNextLine()) {
@@ -42,128 +45,161 @@ public class UserInterface {
 				int friendNum = Integer.parseInt(str[4]);
 				ArrayList<User> friendArray = new ArrayList<>();
 				ArrayList<Interest> userInterest = new ArrayList<>();
+				System.out.println("test:" + friendNum);
+				List tempFriendsList = new List();
 				for(int h = 0; h < friendNum; h++) {
-					User temp = new User(str[h + 5]);
-					friendArray.add(temp);
+					System.out.println(str[h+5]);
+					tempFriendsList.addLast(str[h+5]);
 				}
-				for(int j = 0; j < Integer.parseInt(str[5 + friendNum]); j++) {	
-					
+				tempFriends.add(tempFriendsList);
+				userIndex++;
+				for(int j = 0; j < Integer.parseInt(str[5 + friendNum]); j++) {
+
 					String[] temp = str[j + friendNum + 6].split("#", 2);
 					Interest inter = new Interest(temp[0], Integer.parseInt(temp[1]));
-					
+
 					userInterest.add(inter);
 				}
 				User user = new User(name[0], name[1],str[1], str[2], str[3],++userId, friendArray, userInterest);
-				System.out.println("Chengyun userId: " + user.getUserId());
+				tempUsers.add(name[0] + " " + name[1]);
 				Interest[] temp = user.getInterest();
 				for(int k = 0; k < temp.length; k++) {
 					int index = temp[k].getID();
-					NewFriendByInterest.get(index).insert(user, new NameComparator());			
+					NewFriendByInterest.get(index).insert(user, new NameComparator());
 					if(!(interest.contains(temp[k]))){
 						interest.put(temp[k]);
 					}
 				}
 				UserAccount userAccount = new UserAccount(user);
 				userNames.put(user);
-				userAccounts.put(userAccount);	
-		}	
-		input.close();
+				userAccounts.put(userAccount);
+
+			}
+			input.close();
+
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
-        //login process optionA
+
+		//Rebuild the friend bst for everyone
+		for(int i = 0; i < userNames.getNumElements(); i++) {
+			User tempU = new User(tempUsers.get(i));
+			User currentUser = userNames.get(tempU);
+			for(int j = 0; j < tempFriends.get(i).getLength(); j++) {
+				tempFriends.get(i).placeIterator();
+				User tempF = new User(tempFriends.get(i).getIterator());
+				User currentFriend = userNames.get(tempF);
+				currentUser.addFriend(currentFriend);
+			}
+
+		}
+
+
+		//login process optionA
 		//problem: do not know how to assign a userId to a new user
-        System.out.println("Welcome to Princess system!");
-        
+		System.out.println("Welcome to The Princess System!");
+
 		Scanner input = new Scanner(System.in);
-		System.out.print("\nPlease enter your user name: ");
+		System.out.print("\nPlease enter your Username: ");
 		String userName = input.nextLine();
-		
-		System.out.print("\nPlease enter your password: ");
+
+		System.out.print("\nPlease enter your Password: ");
 		String password = input.nextLine();
-		
+
 		User user = new User(password, userName);
 		UserAccount userAccount = new UserAccount(user);
 		if (!(userAccounts.contains(userAccount))) {
-			System.out.println("\nWe don't have your account on file...\n");
+			System.out.println("\nWe don't have your account on file...");
 			System.out.println("\nLet's create an account for you!");
-			System.out.print("\nEnter your first name: ");
+			System.out.print("\nEnter your First Name: ");
 			String first = input.nextLine();
-			System.out.print("\nEnter your last name: ");
+			System.out.print("\nEnter your Last Name: ");
 			String last = input.nextLine();
 			user.setFirstName(first);
 			user.setLastName(last);
 			user.setUserId(++userId);
-			System.out.println("Chengyun new user userId: " + user.getUserId());
 			outputNames.add(user.getFirstName() + " " + user.getLastName());
 			userNames.put(user);
-			userAccounts.put(userAccount);	
+			userAccounts.put(userAccount);
 		} else {
 			userAccount = userAccounts.get(userAccount);
 			user = userAccount.getUser();
-	}
+		}
 		System.out.println("\nWelcome, " + userAccount.getUser().getFirstName() + " " + userAccount.getUser().getLastName() + "!\n");
-		
-		System.out.println("Welcome to Princess system!");
-	
+
+		System.out.println("Welcome to The Princess System!");
+
 		while(true) {
 			//print menu
-			System.out.println("\nA. Login");
-			System.out.println("B. View My Friends (has sub-menu)");
-			System.out.println("C. Search for a New Friend (has sub-menu)");
-			System.out.println("D. Get Friend Recommendations (has sub-menu)");
-			System.out.println("E. Quit and Write Records to a File\n");
-			System.out.println();
-			System.out.print("Enter your choice: ");
+			System.out.println("\nA. View My Friends");
+			System.out.println("B. Search for a New Friend");
+			System.out.println("C. Get Friend Recommendations");
+			System.out.println("D. Quit and Write Records to a File\n");
+			System.out.print("Enter your choice (A - D): ");
 			String in = input.nextLine();
 			//option B view View My Friends (has sub-menu)
 			// did not work, because we decide only pass name inside user's private bst<user> friends
-			if (in.equals("B"))  {
+			if (in.equals("A"))  {
 				while(true) {
 					System.out.println("\nPlease select from the options below: \n");
-					System.out.println("a. View Friends by name");
-					System.out.println("b. View a friend's profile");	
-					System.out.println("c. Remove a friend from your friend's list");
-					System.out.println("d. Go back to main menu.\n");
-					
+					System.out.println("A. View All Friends by Name");
+					System.out.println("B. View a Friend's Profile");
+					System.out.println("C. Remove a Friend From Your Friends List");
+					System.out.println("D. Back to Main Menu.\n");
+
 					System.out.print("Enter your choice: ");
 					String viewFriend = input.nextLine();
 					System.out.println();
-						if(viewFriend.equals("a")) {
-							user.printUserFriends();
-						}
-						else if(viewFriend.equals("b")) {
-							System.out.print("Enter friend's name: ");
-							String name = input.nextLine();
-							User tempUserFriend = user.getFriendByName(name);
-					        if (tempUserFriend == null){
-					            System.out.println("Sorry, could not find your friend :(");
-					        }
-					        else {
-					            System.out.println(tempUserFriend);
-					        }
-						}
-						else if(viewFriend.equals("c")) {
-							System.out.print("Enter friend's full name: ");
-							String name = input.nextLine();
-							User temp = new User(name);
-					        User tempUserFriend = userNames.get(temp);
-							if(tempUserFriend == null) {
-								System.out.println("Sorry, could not find your friend :(");
-					        }
-							else {
-								user.RemoveFriendByNamed(name);
-							}
-						}
-						else if(viewFriend.equals("d")) {
-							break;
+					if(viewFriend.equals("A")) {
+						if(user.getNumFriends() == 0) {
+							System.out.println("You currently do not have any friends added.");
+							System.out.println("You can (D.) go back to the Main Menu and Add Friends!");
 						}
 						else {
-							System.out.print("invalid input");
+							user.printUserFriends();
 						}
+					}
+					else if(viewFriend.equals("B")) {
+						System.out.print("Enter a Friend's Name: ");
+						String name = input.nextLine();
+						User tempUserFriend = user.getFriendByName(name);
+						if (tempUserFriend == null){
+							System.out.println("Sorry, that User is not in your Friends List!");
+						}
+						else {
+							System.out.println(tempUserFriend);
+						}
+					}
+					else if(viewFriend.equals("C")) {
+						System.out.print("Enter your Friend's Full Name: ");
+						String name = input.nextLine();
+						User temp = new User(name);
+						User tempUserFriend = userNames.get(temp);
+						if(tempUserFriend == null) {
+							System.out.println("Sorry, that User is not in your Friends List!");
+						}
+						else {
+							System.out.println("\nAre you sure you want to remove " + name + " from your Friends List?\n");
+							System.out.print("Yes or No: ");
+							String removeVerification = input.nextLine();
+							if(removeVerification.equals("Yes")) {
+								user.RemoveFriendByNamed(name);
+								System.out.println("\n" + name + " has been removed from your Friends List.\n");
+							}
+							else {
+								return;
+							}
+						}
+					}
+					else if(viewFriend.equals("D")) {
+						break;
+					}
+					else {
+						System.out.print("invalid input");
+					}
 				}
-			}	
-			else if(in.equals("C")) {
+			}
+			else if(in.equals("B")) {
 				while(true) {
 					NameComparator nameC = new NameComparator();
 					System.out.println("\nPlease select from the options below: \n");
@@ -202,7 +238,7 @@ public class UserInterface {
 								else if(viewingFriendChoice.equals("2")) {
 									break;
 								}
-							}	
+							}
 						}
 					}
 					//by interest
@@ -220,13 +256,13 @@ public class UserInterface {
 							//ArrayList<BST<User>> NewFriendByInterest = new ArrayList<BST<User>>();
 							int interestID = foundInterest.getID();
 							if(NewFriendByInterest.get(interestID) == null) {
-								System.out.println("No user has this interest.");
+								System.out.println("No Users are interested in " + interestName + ".");
 							}
 							else {
 								ArrayList<User> interestUserList = NewFriendByInterest.get(interestID).getNodes();
 								String name = "";
 								for(int i = 0; i <interestUserList.size(); i++) {
-									name += interestUserList.get(i).getFirstName() + " " + interestUserList.get(i).getLastName() + "\n";	
+									name += interestUserList.get(i).getFirstName() + " " + interestUserList.get(i).getLastName() + "\n";
 								}
 								System.out.println("\nUsers that also like: " + interestName + "\n\n" + name);
 								System.out.println("\n1. Add Friend");
@@ -243,64 +279,62 @@ public class UserInterface {
 								else if(viewingFriendChoice.equals("2")) {
 									break;
 								}
-							}	
+							}
 						}
 					}
 					else if(addNewFriend.equals("c")) {
 						break;
-					}	
-				}	
+					}
+				}
 			}
-			 else if(in.equals("D")) {
-				 
-			 }
-			 else if (in.equals("E")) {
+			else if(in.equals("C")) {
+
+			}
+			else if (in.equals("D")) {
 				//quit and write records to file OutputPrincesses
 				System.out.println("\nGoodbye!");
 				writeFile("OutputPrincesses.txt", userNames, outputNames);
 				break;
 			} else {
-			System.out.println("\nInvalid menu option. Please enter A-D or E to exit.");
+				System.out.println("\nInvalid menu option. Please enter A-C or D to Save and Exit.");
 			}
 		}
 	}
-	
+
 	public static void writeFile(String fileName, HashTable<User> userNames, ArrayList<String> outputNames){
-        try {
-            PrintWriter out = new PrintWriter(fileName);
-            for(int i = 0; i < outputNames.size(); i++) {
-            	User user = new User(outputNames.get(i));
-            	User outUser = userNames.get(user);
-            	String name = outUser.getFirstName() + " " + outUser.getLastName();
-            	out.println(name);
-            	String userName = outUser.getUserName();
-            	out.println(userName);
-            	String password = outUser.getPassword();
-            	out.println(password);
-            	String city = outUser.getCity();
-            	out.println(city);
-            	int numFriends = outUser.getNumFriends();
-            	out.println(numFriends);
-            	// add name of User' friend
-            	String friendNames = outUser.getAllFriendName();
-            	out.print(friendNames);
-            	
-            	int numInterests = outUser.getNumInterests();
-            	out.println(numInterests);
-            	String interests = "";
-            	for(int j = 0; j < outUser.getInterest().length; j++) {
-            	 	interests += outUser.getInterest()[j].getName() + "#" + outUser.getInterest()[j].getID() + "\n";
-            	}
-            	out.print(interests);
-            	out.println("");
-            }
-            
-            out.flush();
-            out.close();
-        } catch(FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			PrintWriter out = new PrintWriter(fileName);
+			for(int i = 0; i < outputNames.size(); i++) {
+				User user = new User(outputNames.get(i));
+				User outUser = userNames.get(user);
+				String name = outUser.getFirstName() + " " + outUser.getLastName();
+				out.println(name);
+				String userName = outUser.getUserName();
+				out.println(userName);
+				String password = outUser.getPassword();
+				out.println(password);
+				String city = outUser.getCity();
+				out.println(city);
+				int numFriends = outUser.getNumFriends();
+				out.println(numFriends);
+				// add name of User' friend
+				String friendNames = outUser.getAllFriendName();
+				out.print(friendNames);
+
+				int numInterests = outUser.getNumInterests();
+				out.println(numInterests);
+				String interests = "";
+				for(int j = 0; j < outUser.getInterest().length; j++) {
+					interests += outUser.getInterest()[j].getName() + "#" + outUser.getInterest()[j].getID() + "\n";
+				}
+				out.print(interests);
+				out.println("");
+			}
+
+			out.flush();
+			out.close();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
-
-
